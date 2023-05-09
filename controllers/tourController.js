e727d9e1cf71de51a49476cd8c40790f8f1473de
Kamakshee Samant <kamaksheesamant@192-168-1-182.tpgi.com.au>
2023-05-09 15:40:28 +1000

    chore: tours CRUD operations using mongoose

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,59 +1,115 @@
-// const Tour = require('../models/tourModel');
+const Tour = require('../models/tourModel');
 
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // );
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
+exports.getAllTours = async (req, res) => {
+  try {
+    console.log(req.requestTime);
+
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: 'success',
+      requestedAt: req.requestTime,
+      results: tours.length,
+      data: {
+        tours: tours
+      }
+    });
+  } catch (error) {
+    res.status(404).json({
       status: 'fail',
-      message: 'Missing name or price'
+      message: 'error',
+      data: JSON.stringify(error)
     });
   }
-  next();
 };
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
 
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    results: '',
-    data: {
-      tours: ''
+    res.status(200).json({
+      status: 'success',
+      requestedAt: req.requestTime,
+      data: {
+        tour
+      }
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'error',
+      data: JSON.stringify(error)
+    });
+  }
+};
+
+// eslint-disable-next-line node/no-unsupported-features/es-syntax
+exports.createTour = async (req, res) => {
+  // console.log('req body', req.body);
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'error',
+      data: JSON.stringify(error)
+    });
+  }
+};
+
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!tour) {
+      return new Error('No tour found with that ID', 404);
     }
-  });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'error',
+      data: JSON.stringify(error)
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  console.log(req.params);
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndDelete(req.params.id);
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: ''
+    if (!tour) {
+      return new Error('No tour found with that ID', 404);
     }
-  });
-};
 
-exports.createTour = (req, res) => {
-  // console.log(req.body);
-};
-
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<Updated tour here...>'
-    }
-  });
-};
-
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'error',
+      data: JSON.stringify(error)
+    });
+  }
 };
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ mongoose
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  console.log(`App running on port ${port}...${DB}`);
+  console.log(`App running on port ${port}...${DB} okay`);
 });
 
 process.on('unhandledRejection', err => {
