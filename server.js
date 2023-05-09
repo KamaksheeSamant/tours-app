936a753783fd0a24561e8f6e41817cb08ebcb3dd
Kamakshee Samant <kamaksheesamant@192-168-1-182.tpgi.com.au>
2023-05-09 13:44:10 +1000

    chore: db connect via mongoose

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+console.log(">>>>>",DB);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => {
+    console.log(`DB connection successful!`);
+  });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}...${DB}`);
 });
