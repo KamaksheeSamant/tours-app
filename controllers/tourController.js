ce983ee3e3b06f1121cf2a6efb661d5d1a0c4cd5
Kamakshee Samant <kamaksheesamant@192-168-1-182.tpgi.com.au>
2023-05-09 21:17:01 +1000

    chore: add advanced filter functionality

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,16 +1,23 @@
 const Tour = require('../models/tourModel');
-const { removeReservedQueryVals } = require('../utils/common');
+const { filterReservedQueryVals } = require('../utils/common');
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // );
 
 exports.getAllTours = async (req, res) => {
   try {
-    const queryObj = removeReservedQueryVals({ ...req.query });
+    console.log('QUERY::', req.query);
+    const queryObj = filterReservedQueryVals({ ...req.query });
     // Note to self:  <collection_schema>.find always returns the Query Object
     // so u can cahin any other Query methods defined on the query prototypes
     // BUt when u do await , it executes that query
+    // normal query string : ?difficulty=easy&duration=5&page=2
     const query = Tour.find(queryObj);
+
+    // for advanced query
+    // const query = Tour.find({ ratingsAverage: { $gte: 4.7 } });
+    // string : ?difficulty=easy&duration=5&page=2&ratingsAverage[gte]=4.7
+
     const tours = await query;
 
     res.status(200).json({
--- utils/common.js
+++ utils/common.js
@@ -1,9 +1,17 @@
-const removeReservedQueryVals = query => {
+const filterMongoQueryVals = query => {
   // we want to filter few field names from our query strings
-  const queryObj = { ...query };
+  let queryStr = JSON.stringify(query);
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+  return JSON.parse(queryStr);
+};
+
+const filterReservedQueryVals = query => {
+  // we want to filter few field names from our query strings
+  let queryObj = { ...query };
   const excludedFileds = ['page', 'sort', 'limit', 'fields'];
   excludedFileds.forEach(el => delete queryObj[el]);
+  queryObj = filterMongoQueryVals(queryObj);
   return queryObj;
 };
 
-module.exports = { removeReservedQueryVals };
+module.exports = { filterReservedQueryVals };
