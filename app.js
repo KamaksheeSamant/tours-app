f20d00215574ae75684db5a6d6751dc67da70eaa
Kamakshee Samant <kamaksheesamant@192-168-1-182.tpgi.com.au>
2023-05-10 15:27:15 +1000

    chore: handle unknow routes with 404

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 // const { MongoClient, ServerApiVersion } = require('mongodb');
+const AppError = require('./utils/appError');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -29,4 +30,11 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// all the unknow routes needs to eb handled here
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: 'Could not find the route'
+  });
+});
 module.exports = app;
